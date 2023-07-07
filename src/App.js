834919f0a74192d27dec3bb561926f23a22df9cd
Memduh Bayram <memduhbayram0@gmail.com>
2023-07-08 00:03:13 +0300

    react added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import Expenses from "./components/Expenses"
 function App() {
   const expenses = [
@@ -22,12 +22,21 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
-    </div>
-  );
+  
+ //return React.createElement(
+ //  'div',
+ //  {},
+ //  React.createElement('h2', {}, "Let's get started!"),
+ //  React.createElement(Expenses, {items: expenses})
+ //);
+
+  
+ return (
+   <div>
+     <h2>Let's get started!</h2>
+     <Expenses items={expenses}/>
+   </div>
+ );
 }
 
 export default App;
